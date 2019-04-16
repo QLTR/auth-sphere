58280d2da98b0a869f1cbde94256ccb45d21afd4
KevinLoiseau <k.loiseauand@gmail.com>
2019-04-16 15:01:04 +0200

    Initialize basicly serverless eslint and jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: [
+    'src/**/*.js'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+};
